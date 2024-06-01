aef36ce871e5315a9b4d61cd831e94f614b71434
Frederich Blessy <frederichblessy@gmail.com>
2024-06-01 23:44:10 +0700

    Initial Commit

---
+++ BlessqlNative/BlessqlNativeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlessqlNativeApp.swift
+//  BlessqlNative
+//
+//  Created by Frederich Blessy on 01/06/24.
+//
+
+import SwiftUI
+
+@main
+struct BlessqlNativeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
